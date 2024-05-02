2650e668ff6f74ba4d984493876a9b134541945a
Likitha Khatri <likithakhatri5774@gmail.com>
2024-05-02 14:40:56 +0530

    CartContext

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { BsPlus, BsEyeFill } from 'react-icons/bs';
 
 const Product = ({ product }) => {
-  console.log(product);
+  // console.log(product);
   const { id, image, category, title, price } = product;
   return (
     <div>
