700136f647baa345cce577b54e9404383a8f970a
Likitha Khatri <likithakhatri5774@gmail.com>
2024-05-08 22:30:35 +0530

    Hero

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,7 +10,7 @@ import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  const {cart, clearCart, total} = useContext(CartContext);
+  const { cart, clearCart, total } = useContext(CartContext);
 
   return (
     <div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[35vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
@@ -22,7 +22,7 @@ const Sidebar = () => {
       </div>
       <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b'>
         {cart.map((item) => {
-          return <CartItem item={item} key={item.id}/>
+          return <CartItem item={item} key={item.id} />
         })}
       </div>
       <div className='flex flex-col gap-y-3 py-4 mt-4'>
@@ -31,8 +31,12 @@ const Sidebar = () => {
             <span className='mr-2'>Total:</span>$ {parseFloat(total).toFixed(2)}
           </div>
           <div onClick={clearCart} className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center
-           items-center text-xl'><FiTrash2/></div>
+           items-center text-xl'>
+            <FiTrash2 />
+          </div>
         </div>
+        <Link to='/' className='bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium'>View cart</Link>
+        <Link to='/' className='bg-primary flex p-4 justify-center items-center text-white w-full font-medium'>Checkout</Link>
       </div>
     </div>
   )
